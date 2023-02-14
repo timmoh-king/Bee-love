c6098a798d6e3e5a2836379ef61fee0c794a0a90
timmoh-king <timothykariuki43@gmail.com>
2023-02-14 18:59:58 +0300

    Bee_love

---
+++ bee_love/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+      <h1 class="text-3xl font-bold underline">
+          Hello world!
+      </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
