15a39f9052885d8230fa29db0c378bcf2e8f4626
timmoh-king <timothykariuki43@gmail.com>
2023-02-15 13:02:25 +0300

    tailwinds css

--- bee_love/tailwind.config.js
+++ bee_love/tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '480px',
+      'md': '640px',
+      'lg': '768px',
+      'xl': '1024px',
+      '2xl': '1680px',
+    },
+    container: {
+      center: true,
+      padding: '1 rem'
+    },
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#1B0E00",
+        orange: "#F8821E",
+        yellow: "#FAC81A",
+        brown: "#E58A27",
+        dark_brown: "#663A11",
+        background: {
+          1: "#FAD7B7",
+          2: "#FFFBF6",
+          3: "#FFF3E8",
+          4: "#FFE1C5"
+        }
+      }
+    },
   },
   plugins: [],
 }
