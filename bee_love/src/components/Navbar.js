74dfca6684c6ab02435f5f0da745fdc23127d2bb
timmoh-king <timothykariuki43@gmail.com>
2023-02-17 09:30:06 +0300

    navigation bar

---
+++ bee_love/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = ({to}) => {
+    const navlinks = "text-centertext-dark_brown text-normal font-[450] hover:text-orange hover:text-[18px] hover:font-[500]"
+  return (
+    <div>
+      <nav className='relative container p-4'>
+        <div className='flex items-center justify-between'>
+            <div className='h-[40px]'>
+                <img className='h-[65%] md:max-w-[100%] md:h-[75%] lg:h-[100%]' src='honeylogo.png' alt='logo'/>
+            </div>
+            <div className='hidden space-x-6 lg:flex pt-1'>
+                <a href={to} className={navlinks}>Home</a>
+                <a href={to} className={navlinks}>About</a>
+                <a href={to} className={navlinks}>Products</a>
+                <a href={to} className={navlinks}>Services</a>
+                <a href={to} className={navlinks}>Testimonials</a>
+                <a href={to} className={navlinks}>Contact us</a>
+            </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
