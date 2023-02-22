4dbf8273881de3937404146b7812eaa2471a5121
timmoh-king <timothykariuki43@gmail.com>
2023-02-22 13:16:01 +0300

    changes

--- bee_love/src/components/Navbar.js
+++ bee_love/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = ({to}) => {
     const menulinks = "text-center text-dark_brown text-[16px] font-[400] hover:text-orange hover:text-[18px] hover:font-[450]"
   return (
     <div>
-      <nav className='relative container p-4'>
+      <nav className='relative container p-4 bg-transparent'>
         <div className='flex items-center justify-between'>
             <div className='h-[40px]'>
                 <img className='h-[65%] md:max-w-[100%] md:h-[75%] lg:h-[100%]' src='honeylogo.png' alt='logo'/>
