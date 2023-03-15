8fb79698c176b269c0fe14441f8843f8c5e70b55
timmoh-king <timothykariuki43@gmail.com>
2023-03-15 13:15:02 +0300

    app js

--- bee_love/src/App.js
+++ bee_love/src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Aboutpage from "./components/pages/Aboutpage";
+import Contactpage from "./components/pages/Contactpage";
+import Productpage from "./components/pages/Productpage";
+import Servicespage from "./components/pages/Servicespage";
+import Testimonialpage from "./components/pages/Testimonialpage";
 import Webpage from "./components/pages/Webpage";
 
 function App() {
   return (
     <div>
-      <Webpage />
+      <Routes>
+      <Route path="/" element={<Webpage />} />
+        <Route path="about" element={<Aboutpage />} />
+        <Route path="products" element={<Productpage  />} />
+        <Route path="services" element={<Servicespage />} />
+        <Route path="testimonials" element={<Testimonialpage />} />
+        <Route path="contact" element={<Contactpage />} />
+      <Route path="*" element={<Webpage />} />
+      </Routes>
     </div>
   );
 }
